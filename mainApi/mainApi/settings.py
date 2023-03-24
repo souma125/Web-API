2e353818f4681b0e1ec19a7e66310b75a503396d
souma125 <53054888+souma125@users.noreply.github.com>
2023-03-24 14:42:48 +0530

    Fourth commit

--- mainApi/mainApi/settings.py
+++ mainApi/mainApi/settings.py
@@ -136,3 +136,13 @@ CORS_ALLOWED_ORIGINS = [
 ]
 
 
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    # 'DEFAULT_PERMISSION_CLASSES': [
+    #     'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    # ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
